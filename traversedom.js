fa31e3a9feb2f1dda70aceabe0ce7fd6bc665181
Vel E <ve23@njit.edu>
2016-08-05 16:43:26 -0400

    Delete traversedom.js

--- traversedom.js
+++
@@ -1,9 +0,0 @@
-function update(){
-  $("span").css("background-color", "lightgrey");
-  $("div").each(function(i){
-    var $input = $(this).children("input:first");
-    var $value = $input.val();
-    var filter = "span:lt(" + $value + ")";
-    $input.siblings(filter).css("background-color", "blue");
-  })
-}
